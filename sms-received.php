3da09d5ffead516e8c68cf466afaa7b1e996e5d0
David Menache <dmenache@devopsandplatforms.com>
2024-11-23 17:44:14 +0000

    sms-received.php added

---
+++ sms-received.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $to = "hank@doap.com";
+    $subject = "Inbound SMS Alert";
+    $message = "Hello Team, this is a message from the PBX alerting you to an inbound SMS.  
+
+Check it out at:
+	https://sip.doap.com/ucp/
+
+!";
+    $headers = "From: info@doap.com";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "SMS alert sent successfully to Team!";
+    } else {
+        echo "Failed to send email to Team. Please try again later.";
+    }
+} else {
+    echo "Invalid request method!";
+}
+?>
